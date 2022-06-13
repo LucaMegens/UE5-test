43f23fb780d2e469f1fd21624e3e296e4983de31
Luca Megens <mail@lucamegens.nl>
2022-06-13 14:33:10 +0200

    Project files uploaded

---
+++ AudioVisualizer4/Intermediate/Source/AudioVisualizer4.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class AudioVisualizer4 : ModuleRules
+{
+	public AudioVisualizer4(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
